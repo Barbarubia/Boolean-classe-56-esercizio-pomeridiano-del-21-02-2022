aa4adc815bb19ab44f664c0b4332c116268d1680
Barbarubia <dnl.desimone@gmail.com>
2022-02-21 11:07:04 -0300

    inserito arrotondamento decimali

--- js/script.js
+++ js/script.js
@@ -8,19 +8,19 @@ let outputMessage = document.getElementById('ticket-price');
 
 if (age < 18) {
 
-    let finalPrice = standardPrice * 0.8;
+    let finalPrice = (Math.round(standardPrice * 0.8 * 100) / 100).toFixed(2);
 
     outputMessage.innerHTML = `<h3>Avendo ${age} anni hai diritto allo sconto del 20%!</h3><h3>Il prezzo del tuo biglietto è di ${finalPrice}&euro;</h3><button>Compra il biglietto</button>`;
 
 } else if (age >= 65) {
 
-    let finalPrice = standardPrice * 0.6;
+    let finalPrice = (Math.round(standardPrice * 0.6 * 100) / 100).toFixed(2);
 
     outputMessage.innerHTML = `<h3>Avendo ${age} anni hai diritto allo sconto del 40%!</h3><h3>Il prezzo del tuo biglietto è di ${finalPrice}&euro;</h3><button>Compra il biglietto</button>`;
 
 } else {
 
-    let finalPrice = standardPrice;
+    let finalPrice = (Math.round(standardPrice * 100) / 100).toFixed(2);
 
     outputMessage.innerHTML = `<h3>Il prezzo del tuo biglietto è di ${finalPrice}&euro;</h3><button>Compra il biglietto</button>`;
 
